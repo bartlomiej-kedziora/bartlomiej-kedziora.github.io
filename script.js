cf6b2db20d85c7a43e55e40aa14697ce72af8465
bartlomiej-kedziora <40625440+bartlomiej-kedziora@users.noreply.github.com>
2018-10-11 00:40:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   const apiRoot = 'https://tasks-aku.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://tasks-aku.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://tasks-aku.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
